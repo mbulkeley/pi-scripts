dd22cf11cb919d5ec28144af7c811c3e8517167b
mbulkeley <3985232+mbulkeley@users.noreply.github.com>
2026-04-17 18:17:33 +0200

    Fix PATH in speedtest script; restore cron to 17:30

--- speedtest/speedtest_logger.sh
+++ speedtest/speedtest_logger.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 source /etc/environment
 LOGFILE="/home/pi/projects/speedtest/logs/speedtest_log.csv"
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
