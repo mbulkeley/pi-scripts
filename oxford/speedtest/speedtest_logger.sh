1178696501b8bddaefc5b1ce4fc1a56aa6036181
mbulkeley <3985232+mbulkeley@users.noreply.github.com>
2026-04-24 17:58:51 +0200

    Fix OXFORD speedtest: env var loading, logfile path, add temp

--- oxford/speedtest/speedtest_logger.sh
+++ oxford/speedtest/speedtest_logger.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-source /etc/environment
-LOGFILE="$HOME/Projects/speedtest/speedtest_log.csv"
+export SLACK_WEBHOOK_OXFORD=$(grep SLACK_WEBHOOK_OXFORD /etc/environment | cut -d= -f2)
+LOGFILE="/home/pi/Projects/pi-scripts/oxford/speedtest/logs/speedtest_log.csv"
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Run test and extract useful fields
 RESULT=$(/usr/local/bin/speedtest --format=json --accept-license --accept-gdpr)
+TEMP=$(vcgencmd measure_temp | grep -o '[0-9]*\.[0-9]*')
 PING=$(echo $RESULT | jq '.ping.latency')
 DOWNLOAD=$(echo $RESULT | jq '.download.bandwidth' | awk '{print $1/125000}')
 UPLOAD=$(echo $RESULT | jq '.upload.bandwidth' | awk '{print $1/125000}')
@@ -24,4 +25,4 @@ SLACK_WEBHOOK="${SLACK_WEBHOOK_OXFORD}"
 
 curl -s -X POST "$SLACK_WEBHOOK" \
   -H 'Content-type: application/json' \
-  --data "{\"text\":\"📡 *Speedtest Results - $HOSTNAME*\n*Date:* $DATE\n*Ping:* ${PING}ms\n*Download:* ${DOWNLOAD} Mbps\n*Upload:* ${UPLOAD} Mbps\n*ISP:* $ISP\"}"
+  --data "{\"text\":\"📡 *Speedtest Results - $HOSTNAME*\n*Date:* $DATE\n*Ping:* ${PING}ms\n*Download:* ${DOWNLOAD} Mbps\n*Upload:* ${UPLOAD} Mbps\n*ISP:* $ISP\n*Temp:* ${TEMP}°C\"}"
